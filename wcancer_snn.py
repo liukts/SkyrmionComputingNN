27d2cde03aa0c885ee10b44f1bda362f82a9d91f
liukts <liukts@utexas.edu>
2021-05-25 15:00:07 -0500

    model update

--- wcancer_snn.py
+++ wcancer_snn.py
@@ -11,6 +11,13 @@ import torch.nn.init as init
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+from norse.torch import LIFParameters, LIFState
+from norse.torch.module.lif import LIFCell, LIFRecurrentCell
+# Notice the difference between "LIF" (leaky integrate-and-fire) and "LI" (leaky integrator)
+from norse.torch import LICell, LIState
+
+from typing import NamedTuple
+
 # load data, process into tensor
 data = pd.read_csv("./wcancer_data.csv")
 x = data.drop(["id", "diagnosis", "Unnamed: 32"], axis=1)  # prune unused data
@@ -24,3 +31,71 @@ x_test_trans = scaler.fit_transform(x_test)
 train = data_utils.TensorDataset(torch.from_numpy(x_train_trans).float(),
                                  torch.from_numpy(y_train.as_matrix()).float())
 dataloader = data_utils.DataLoader(train, batch_size=128, shuffle=False)
+
+class SNNState(NamedTuple):
+    lif0 : LIFState
+    readout : LIState
+
+
+class SNN(torch.nn.Module):
+    def __init__(self, input_features, hidden_features, output_features, record=False, dt=0.001):
+        super(SNN, self).__init__()
+        self.l1 = LIFCell(
+            input_features,
+            hidden_features,
+            p=LIFParameters(alpha=100, v_th=torch.tensor(0.5)),
+            dt=dt             
+        )
+        self.input_features = input_features
+        self.fc_out = torch.nn.Linear(hidden_features, output_features, bias=False)
+        self.out = LICell(dt=dt)
+
+        self.hidden_features = hidden_features
+        self.output_features = output_features
+        self.record = record
+
+    def forward(self, x):
+        seq_length, batch_size, _, _, _ = x.shape
+        s1 = so = None
+        voltages = []
+
+        if self.record:
+          self.recording = SNNState(
+              LIFState(
+                z = torch.zeros(seq_length, batch_size, self.hidden_features),
+                v = torch.zeros(seq_length, batch_size, self.hidden_features),
+                i = torch.zeros(seq_length, batch_size, self.hidden_features)
+              ),
+              LIState(
+                v = torch.zeros(seq_length, batch_size, self.output_features),
+                i = torch.zeros(seq_length, batch_size, self.output_features)
+              )
+          )
+
+        for ts in range(seq_length):
+            z = x[ts, :, :, :].view(-1, self.input_features)
+            z, s1 = self.l1(z, s1)
+            z = self.fc_out(z)
+            vo, so = self.out(z, so)
+            if self.record:
+              self.recording.lif0.z[ts,:] = s1.z
+              self.recording.lif0.v[ts,:] = s1.v
+              self.recording.lif0.i[ts,:] = s1.i
+              self.recording.readout.v[ts,:] = so.v
+              self.recording.readout.i[ts,:] = so.i
+            voltages += [vo]
+
+        return torch.stack(voltages)
+
+class Model(torch.nn.Module):
+    def __init__(self, encoder, snn, decoder):
+        super(Model, self).__init__()
+        self.encoder = encoder
+        self.snn = snn
+        self.decoder = decoder
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.snn(x)
+        log_p_y = self.decoder(x)
+        return log_p_y
